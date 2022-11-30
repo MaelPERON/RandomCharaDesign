413625e4cc0f8d9eaa624112a1e33fd34b34cb34
MaelPERON <maelperonmessagerie2015@gmail.com>
2022-11-30 01:25:26 +0100

    Moved switch structures to separate function

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ function move() {}
 function stop() {}
 function toggleFilter() {}
 function togglePreview() {}
+function newAction(e) {
+    obj = e.currentTarget;
+    switch(obj.getAttribute('action')) {
+        case "start":
+        case "play":
+        case "resume":
+        case "stop":
+        case "next":
+        case "toggle-filter":
+        case "toggle-preview":
+        case "previous":
+        case "edit-time":
+            alert(obj.getAttribute('action'))
+            break;
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     toggleFilter()
@@ -17,27 +33,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     actions = Array.from(document.querySelectorAll('[action]'))
     actions.filter(a => a.tagName != "INPUT").forEach(action => {
-        action.addEventListener('click', (e) => {
-            e.preventDefault();
-            switch(e.currentTarget.getAttribute('action')) {
-                case "start":
-                case "play":
-                case "resume":
-                case "stop":
-                case "next":
-                case "toggle-filter":
-                case "previous":
-                    break;
-            }
-        })
+        action.addEventListener('click', (e) => { e.preventDefault(); newAction(e); })
     })
     actions.filter(a => a.tagName == "INPUT").forEach(action => {
-        action.addEventListener('change', (e) => {
-            switch(e.currentTarget.getAttribute('action')) {
-                case "toggle-preview":
-                case "edit-time":
-                    break;
-            }
-        })
+        action.addEventListener('change', (e) => { newAction(e); })
     })
 })
