779abc646e6954fa455deee19482162a01ce74c6
MaelPERON <maelperonmessagerie2015@gmail.com>
2022-11-30 01:06:23 +0100

    Initialized app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+var images = [];
+
+function restart() {}
+function togglePause() {}
+function move() {}
+function stop() {}
+function toggleFilter() {}
+function togglePreview() {}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+})
