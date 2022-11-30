1caf7d49aac7263fad20237ec4d2e712d4e0b155
MaelPERON <maelperonmessagerie2015@gmail.com>
2022-11-30 01:19:58 +0100

    Created actions switch's structure

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var images = [];
+var actions = [];
+var inputs = [];
+var filterOn = true;
+var previewOn = true;
 
 function restart() {}
 function togglePause() {}
@@ -8,5 +12,32 @@ function toggleFilter() {}
 function togglePreview() {}
 
 document.addEventListener("DOMContentLoaded", () => {
+    toggleFilter()
+    togglePreview()
 
+    actions = Array.from(document.querySelectorAll('[action]'))
+    actions.filter(a => a.tagName != "INPUT").forEach(action => {
+        action.addEventListener('click', (e) => {
+            e.preventDefault();
+            switch(e.currentTarget.getAttribute('action')) {
+                case "start":
+                case "play":
+                case "resume":
+                case "stop":
+                case "next":
+                case "toggle-filter":
+                case "previous":
+                    break;
+            }
+        })
+    })
+    actions.filter(a => a.tagName == "INPUT").forEach(action => {
+        action.addEventListener('change', (e) => {
+            switch(e.currentTarget.getAttribute('action')) {
+                case "toggle-preview":
+                case "edit-time":
+                    break;
+            }
+        })
+    })
 })
